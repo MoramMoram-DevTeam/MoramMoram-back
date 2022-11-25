68830d97351e6b6af2b66429de3d49b86a429c11
Hyun Woo <hwsa10041@gmail.com>
2022-11-25 22:41:23 +0900

    deploy 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PROJECT_ROOT="/home/ec2-user/MoramMoram-back"
+PROJECT_ROOT="/home/ec2-user/app/deploy"
 JAR_FILE="$PROJECT_ROOT/build/libs/MoramMoram-Server-0.0.1-SNAPSHOT.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
