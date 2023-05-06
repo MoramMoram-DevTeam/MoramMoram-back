060b4ac3a75ddcf5d1ac6fdb281fd5b8032571fe
Hyun Woo <hwsa10041@gmail.com>
2023-05-06 14:56:23 +0900

    [CHORE] 불필요한 쿼리 구문 삭제

--- src/main/java/kusitms/candoit/MoramMoramServer/domain/fleaMarket/Repository/FleamarketRepository.java
+++ src/main/java/kusitms/candoit/MoramMoramServer/domain/fleaMarket/Repository/FleamarketRepository.java
@@ -11,12 +11,6 @@ import java.util.List;
 public interface FleamarketRepository extends JpaRepository<Fleamarket, Long> {
     List<Fleamarket> findByMarketNameContaining(String m_name);
     Fleamarket findFleamarketById(Long m_id);
-
-    @Transactional
-    @Modifying
-    @Query("update Fleamarket f set f.views = f.views + 1 where f.id = :id")
-    int updateViewCount(Long id);
-
     List<Fleamarket> findTop10ByOrderByViewsDesc();
 //    findTop300By OrderBy SeqDesc
 }
